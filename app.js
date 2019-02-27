f36f95434c3f1b0a087d8c2716ebd04cf8eee5ee
Mike DeGan <mikedegan@gmail.com>
2019-02-26 20:32:08 -0500

    ready for section 9

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
