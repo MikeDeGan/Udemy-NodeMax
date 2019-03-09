5ef707eb1111dd5063ed1293a011fb222a66625c
Mike DeGan <mikedegan@gmail.com>
2019-03-08 21:59:48 -0500

    section 9 complete

--- routes/admin.js
+++ routes/admin.js
@@ -15,6 +15,9 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+//delete product
+router.post('/delete-product', adminController.postDeleteProduct);
+
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
 router.post('/edit-product', adminController.postEditProduct);
